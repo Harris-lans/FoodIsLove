25ad14b30efab4189ae5af408308071d53359e94
Harish Kumar <harishsghp@gmail.com>
2018-10-03 15:40:13 -0700

    Cooking Steps UI Update Implemented (Testing to be done after enough data has been collected)

---
+++ Assets/Scripts/UI/InGameUI/IngredientContainer.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientContainer : MonoBehaviour 
+{
+    #region Member Variables
+
+        [Header("References")]
+        [SerializeField]
+        private CookingStepsIcon _CookingStepsIconPrefab;
+        [SerializeField]
+        private Image _IngredientImage;
+        [SerializeField]
+        private RectTransform _StepsContainer;
+
+        [Header("Events to listen to")]
+        [SerializeField]
+        private SO_GenericEvent _IngredientAddedToCookingPotEvent;
+
+        private SO_Tag _Ingredient;
+        private SO_Tag[] _CookingStepsToTrack;
+        private List<CookingStepsIcon> _CookingStepsIcon;
+        private SO_IngredientData _IngredientData;
+
+    #endregion 
+
+    #region Life Cycle
+
+        private void Start() 
+        {
+            _IngredientAddedToCookingPotEvent.AddListener(OnIngredientAddedToCookingPot);
+            _CookingStepsIcon = new List<CookingStepsIcon>();
+            _IngredientData = Resources.Load<SO_IngredientData>("IngredientsData");
+        }
+
+    #endregion
+
+    #region Member Functions
+
+        public void Initialize(SO_Tag ingredient, SO_Tag[] cookingSteps) 
+        {
+            _CookingStepsToTrack = cookingSteps;
+            _Ingredient = ingredient;
+
+            foreach(var cookingStep in _CookingStepsToTrack)
+            {
+                // Adding step icons to the container
+                var cookingStepIcon = Instantiate(_CookingStepsIconPrefab, _StepsContainer); 
+                cookingStepIcon.Initialize(cookingStep);
+                _CookingStepsIcon.Add(cookingStepIcon);
+            }
+
+            // Displaying the ingredient icon
+            _IngredientImage.sprite = _IngredientData.GetIngredientIcon(_Ingredient);
+        }
+
+        private void OnIngredientAddedToCookingPot(object cookingStepData)
+        {
+            // Check if the ingredient matches and if it matches update the cooking step
+            CookedIngredient cookedIngredient = (CookedIngredient)cookingStepData;
+
+            // Checking if this was the ingredient cooked
+            if ( cookedIngredient != null && cookedIngredient.Ingredient == _Ingredient)
+            {
+                // Also checking if all the ingredient steps are completed
+                bool ingredientCompletelyCooked = false;
+
+                foreach(var cookingStepIcon in _CookingStepsIcon)
+                {
+                    cookingStepIcon.ValidateAndUpdate(cookedIngredient.CookingMethod);
+                    ingredientCompletelyCooked &= cookingStepIcon.IsCompleted;
+                }
+
+                if (ingredientCompletelyCooked)
+                {
+                    MarkAsCompleted();
+                }
+            }
+        }
+
+        private void MarkAsCompleted()
+        {
+            // TODO: Grey out the ingredient image to show that it has been cooked
+            _IngredientImage.color = Color.gray;
+
+            // Stopping to listen to the Cooking Pot event
+            _IngredientAddedToCookingPotEvent.RemoveListener(OnIngredientAddedToCookingPot);
+        }
+    #endregion
+}
