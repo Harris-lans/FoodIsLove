5b7ab111f757be038e0818409969269fb74e8681
Harish Kumar <harishsghp@gmail.com>
2018-10-24 20:04:09 -0700

    Spawn Particles implemented

---
+++ Assets/Scripts/Utility/ParticlesOnSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesOnSpawn : MonoBehaviour 
+{
+	[SerializeField]
+	private GameObject _SpawnParticles;
+
+	void Start () 
+	{
+		Instantiate(_SpawnParticles, transform.position, Quaternion.identity);	
+	}
+}
