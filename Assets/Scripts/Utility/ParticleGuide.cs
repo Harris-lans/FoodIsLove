37d7768d9e5241527333540fb5e2a5c7f7fb9467
Harish Kumar (School) <harishsghp@gmail.com>
2018-11-03 20:49:22 -0700

    New Glow Effect

---
+++ Assets/Scripts/Utility/ParticleGuide.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleGuide : MonoBehaviour
+{
+
+
+    [SerializeField]
+    int SpeedMult = 10;
+    [SerializeField]
+    float ParticleDelay = 2.0f;
+    public ParticleSystem Particlefollow;
+    public Transform Target;
+
+
+    private void Start()
+    {
+
+        Particlefollow = GetComponent<ParticleSystem>();
+		StartCoroutine(TimeDelay());
+    }
+
+    private IEnumerator TimeDelay()
+    {
+        yield return new WaitForSeconds(ParticleDelay);
+
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[Particlefollow.particleCount];
+
+		while (true)
+		{
+			Debug.Log("Working");
+			particles = new ParticleSystem.Particle[Particlefollow.particleCount];
+			Particlefollow.GetParticles(particles);
+
+			// Modifying the particles
+
+			for (int i = 0; i < particles.GetLength(0); i++)
+			{
+				float ForceToAdd = (particles[i].startLifetime - particles[i].remainingLifetime) * (SpeedMult * Vector3.Distance(Target.position, particles[i].position));
+				particles[i].velocity = (Target.position - particles[i].position).normalized * ForceToAdd;
+			}
+
+			Particlefollow.SetParticles(particles, particles.Length);
+
+			yield return null;
+		}
+    }
+}
