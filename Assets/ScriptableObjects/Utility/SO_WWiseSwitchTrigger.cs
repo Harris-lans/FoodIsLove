37906b376a4821473b604ad9a19e5cab261615ed
Harish Kumar (School) <harishsghp@gmail.com>
2018-11-07 17:24:00 -0800

    New Optimus Pine Animations

--- Assets/ScriptableObjects/Utility/SO_WWiseSwitchTrigger.cs
+++ Assets/ScriptableObjects/Utility/SO_WWiseSwitchTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName="FoodIsLove/WWise/WWiseSwitchTrigger", fileName = "WWiseSwitchTrigger")]
-public class SO_WWiseSwitchTrigger : MonoBehaviour 
+public class SO_WWiseSwitchTrigger : ScriptableObject 
 {
 	public string SwitchGroup;
 	public string SwitchState;
