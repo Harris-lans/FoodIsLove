0f9e7e638449ecdb6338c658f89d0ec0497b6ca2
Harish Kumar <harishsghp@gmail.com>
2018-11-19 17:41:23 -0800

    Fixed the progressive music

--- Assets/ScriptableObjects/Recipe/SO_Recipe.cs
+++ Assets/ScriptableObjects/Recipe/SO_Recipe.cs
@@ -7,4 +7,20 @@ public class SO_Recipe : ScriptableObject
 {
     [Header("Create your own recipe")]
 	public CookedIngredient[] IngredientsList;
+
+    public Dictionary<SO_Tag, List<SO_Tag>> GetRecipeDictionary()
+    {
+        Dictionary<SO_Tag, List<SO_Tag>> recipeDictionary = new Dictionary<SO_Tag, List<SO_Tag>>();
+
+        foreach (var ingredientStep in IngredientsList)
+        {
+            if (!recipeDictionary.ContainsKey(ingredientStep.Ingredient))
+            {
+                recipeDictionary[ingredientStep.Ingredient] = new List<SO_Tag>();
+            }
+            recipeDictionary[ingredientStep.Ingredient].Add(ingredientStep.CookingMethod);
+        }
+
+        return recipeDictionary; 
+    }
 }
---
+++ Assets/Scripts/Utility/GameObjectTools.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectTools : MonoBehaviour 
+{
+
+	public void DestroyObject()
+	{
+		Destroy(gameObject);
+	}
+}
